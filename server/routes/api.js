ac8095faa71e9e7e7b53bfe9291c5a9cdbd95c83
Andrew Lubega <67166869+andrewLubega@users.noreply.github.com>
2024-12-13 13:12:56 -0800

    fix

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getReview, getReviews, createReview, deleteReview } from '../data/reviews.js';
+import { createReview, deleteReview } from '../data/reviews.js';
 import { getVenuesById } from '../data/reviews.js';
 import { getReviewById } from '../data/reviews.js';
 //MUKISA
